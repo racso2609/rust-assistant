a40a26f88b4697d7db7bd61c442eb9f1068026ba
racso2609 <oscaremiliolugo@gmail.com>
2023-09-03 12:48:40 -0400

    add lib

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use hello_rust::text_bot::invoke_crab;
+// use hello_rust::text_bot::invoke_crab;
 use dotenv::dotenv;
+use hello_rust::speech::get_audio;
 
 fn main() {
     dotenv().ok();
-
-    let message = "yarvis reproduce un verano sin ti";
-    invoke_crab(message);
+    get_audio().ok();
+    // let message = "yarvis reproduce un verano sin ti";
+    // invoke_crab(message);
 }
